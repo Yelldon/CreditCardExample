fac9396243ea5842e69ad3bfaefad9def7b3475d
Greg Patrick <greg.hello@gmail.com>
2025-07-20 14:21:12 -0500

    Initial Commit

---
+++ CreditCardExample/CreditCardExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardExampleApp.swift
+//  CreditCardExample
+//
+//  Created by Greg Patrick on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CreditCardExampleTests/CreditCardExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardExampleTests.swift
+//  CreditCardExampleTests
+//
+//  Created by Greg Patrick on 7/20/25.
+//
+
+import Testing
+@testable import CreditCardExample
+
+struct CreditCardExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
